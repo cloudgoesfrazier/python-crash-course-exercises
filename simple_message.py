24a803fb775cd9dcfe7446138fe97236ed6b8202
cloudgoesfrazier <zach@cloudgoesfrazier.com>
2024-04-25 22:36:47 -0400

    Chapter 2 Simple Message Excercises (2.1 and 2.2)

---
+++ simple_message.py
@@ -0,0 +1,11 @@
+#2-1. Excercise Assign a message to a variable, and then print that message.
+message = "Test"
+print(message)
+
+#2.2 Simple Messages: Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
+
+simple_message = "Original Message"
+print(simple_message)
+
+simple_message = "New Message"
+print(simple_message)
